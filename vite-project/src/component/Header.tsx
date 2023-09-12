18c81b980facf150c9896de121948c1b3406e453
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-12 13:17:09 +0100

    Working with harmburger section

--- vite-project/src/component/Header.tsx
+++ vite-project/src/component/Header.tsx
@@ -1,27 +1,62 @@
 import Classes from "../sass/header.module.scss";
-import React from "react";
 import { Link } from "react-router-dom";
+import { FaTimes, FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+
+  function showTimes() {
+    setClick(false);
+  }
+
+  function showHamburger() {
+    setClick(true);
+  }
+
   return (
     <>
-      <main>
+      <nav className={Classes["main-header-section"]}>
         <h1>Portfolio.</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/project">Project</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </main>
+        <section className={Classes["navLists"]}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/project">Project</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </section>
+        <div className={Classes["hamburger-section"]}>
+          {!click && (
+            <FaBars
+              onClick={showHamburger}
+              style={{
+                color: "white",
+                cursor: "Pointer",
+                fontSize: "20px",
+              }}
+            />
+          )}
+          {click && (
+            <FaTimes
+              onClick={showTimes}
+              style={{
+                color: "white",
+                cursor: "Pointer",
+                fontSize: "20px",
+              }}
+            />
+          )}
+        </div>
+      </nav>
     </>
   );
 };
