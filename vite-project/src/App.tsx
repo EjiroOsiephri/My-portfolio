8a71e8fbba251fd27173589ed7a229e40cb50dbe
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-18 20:36:00 +0100

    Added laxy loading of pages

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,18 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Project from "./pages/Project";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
+import React, { Suspense } from "react";
+import LoadingSpinner from "./component/LoadingSpinner";
 
 function App() {
+  const Home = React.lazy(() => import("./pages/Home"));
+  const Project = React.lazy(() => import("./pages/Project"));
+  const About = React.lazy(() => import("./pages/About"));
+  const Contact = React.lazy(() => import("./pages/Contact"));
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/project" element={<Project />}></Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="spinner">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/project" element={<Project />}></Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
---
+++ vite-project/src/component/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import classes from "../sass/LoadingSpinner.module.scss";
+
+const LoadingSpinner = () => {
+  return <div className={classes.spinner}></div>;
+};
+
+export default LoadingSpinner;
