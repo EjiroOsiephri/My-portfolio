69f538c6a2aee92a9cf572c231eb15c08a2d01bc
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-18 09:22:18 +0100

    introducing react simple typewriter

--- vite-project/src/component/ContactHero.tsx
+++ vite-project/src/component/ContactHero.tsx
@@ -1,8 +1,13 @@
 import HeroImg from "../assets/unsplash-KwJ3FEuwRlE-unsplash.jpg";
 import Classes from "../sass/Hero.module.scss";
-import { Link } from "react-router-dom";
+import { useTypewriter } from "react-simple-typewriter";
 
-const HeroSection = () => {
+const ContactSection = () => {
+  const [text] = useTypewriter({
+    words: ["Let's have a chat shall we?"],
+    loop: 0,
+  });
+
   return (
     <>
       <main className={Classes["hero"]}>
@@ -10,18 +15,18 @@ const HeroSection = () => {
           <img src={HeroImg} className={Classes["hero-img"]} alt="" />
         </div>
         <div className={Classes["content"]}>
-          <p>Hi, I'm Ejiro</p>
-          <h1>MERN STACK DEVELOPER</h1>
-          <button className={Classes["btn"]}>
-            <Link to="/project">Projects</Link>
-          </button>
-          <button className={Classes["btn-light"]}>
-            <Link to="/contact">Contacts</Link>
-          </button>
+          <h1>Contact.</h1>
+          <p
+            style={{
+              textTransform: "initial",
+            }}
+          >
+            {text}
+          </p>
         </div>
       </main>
     </>
   );
 };
 
-export default HeroSection;
+export default ContactSection;
--- vite-project/src/pages/Contact.tsx
+++ vite-project/src/pages/Contact.tsx
@@ -1,9 +1,11 @@
+import ContactSection from "../component/ContactHero";
 import Header from "../component/Header";
 
 const Contact = () => {
   return (
     <>
       <Header />
+      <ContactSection />
     </>
   );
 };
